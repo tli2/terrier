b555b7fc2302919bfa965bce47b42b5941ec5889
Tianyu Li <tli2@andrew.cmu.edu>
2019-04-27 19:13:25 -0400

    strip out network as well

--- src/include/network/rdma/data_format.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdint>
-#include <cstdlib>
-
-struct size_pair {
-  size_t data_size;      // size of final data (table)
-  size_t metadata_size;  // size of metadata
-} __attribute__ ((packed));
--- src/include/network/rdma/server.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "storage/data_table.h"
-#include "network/rdma/rdma.h"
-
-int do_rdma(int sockfd, terrier::storage::DataTable *datatable);
-int do_send(struct resources *res, char *buf, size_t buf_size, uint64_t remote_addr);
