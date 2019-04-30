89549a958d5b49f689de0ae1f6e68fe01f0aa2cd
Tianyu Li <tli2@andrew.cmu.edu>
2019-04-30 19:18:42 -0400

    hacky vectorized version of code

--- src/main/terrier.cpp
+++ src/main/terrier.cpp
@@ -37,6 +37,62 @@
 #define ONE_MEGABYTE 1048576
 
 namespace terrier {
+char write_buf[common::Constants::BLOCK_SIZE * 50];
+
+uint32_t sizes[10] = {8u, 8u, 4u, 4u, 1u, 1u, 1u, 1u, 1u, -1u};
+struct BlockWriter {
+  void ResetWrite(uint32_t batch_size) {
+    num_tuples_in_batch = batch_size;
+    col_starts[0] = col_write_heads[0] = 2 * sizeof(uint32_t);
+    for (uint32_t i = 1; i < 10; i++)
+      col_starts[i] = col_write_heads[i] = col_write_heads[i - 1] + sizes[i - 1] * batch_size;
+  }
+
+  void WriteAttribute(byte *entry, uint32_t size, uint32_t col_num) {
+    if (col_num == 9) throw std::runtime_error("Varlen written as fixed length");
+    if (entry != nullptr)
+      std::memcpy(write_buf + (col_write_heads[col_num]), entry, size);
+    col_write_heads[col_num] += size;
+  }
+
+  void WriteVarlen(storage::VarlenEntry *varlen) {
+    if (varlen == nullptr) {
+      uint32_t size = 0;
+      std::memcpy(write_buf + (col_write_heads[9]), &size, sizeof(uint32_t));
+      col_write_heads[9] += sizeof(uint32_t);
+      return;
+    }
+    uint32_t size = varlen->Size();
+    std::memcpy(write_buf + (col_write_heads[9]), &size, sizeof(uint32_t));
+    col_write_heads[9] += sizeof(uint32_t);
+    std::memcpy(write_buf + (col_write_heads[9]), varlen->Content(), size);
+    col_write_heads[9] += size;
+  }
+
+  void FlushBatch(int fd) {
+    uint32_t num_tuples = (col_write_heads[0] - col_starts[0]) / 8;
+    if (num_tuples != num_tuples_in_batch) {
+      // underfilled. slow path
+      uint32_t total_size = 0;
+      storage::PosixIoWrappers::WriteFully(fd, &num_tuples, sizeof(uint32_t));
+      for (uint32_t i = 0; i < 10; i++)
+        total_size += col_write_heads[i] - col_starts[i];
+      storage::PosixIoWrappers::WriteFully(fd, &total_size, sizeof(uint32_t));
+      for (uint32_t i = 0; i < 10; i++)
+        storage::PosixIoWrappers::WriteFully(fd, write_buf + (col_starts[i]), col_write_heads[i] - col_starts[i]);
+    } else {
+      uint32_t total_size = col_write_heads[9] - 2 * sizeof(uint32_t);
+      std::memcpy(write_buf, &num_tuples_in_batch, sizeof(uint32_t));
+      std::memcpy(write_buf + sizeof(uint32_t), &total_size, sizeof(uint32_t));
+      storage::PosixIoWrappers::WriteFully(fd, write_buf, col_write_heads[9]);
+    }
+  }
+
+  uint32_t num_tuples_in_batch;
+  uint32_t col_starts[10];
+  uint32_t col_write_heads[10];
+};
+
 class TpccLoader {
  public:
   void StartGC(transaction::TransactionManager *const) {
@@ -78,13 +134,14 @@ class TpccLoader {
   storage::AccessObserver access_observer_{&compactor_};
 
   const int8_t num_threads_ = 2;
-  const uint32_t num_precomputed_txns_per_worker_ = 5000000;
+  const uint32_t num_precomputed_txns_per_worker_ = 500;
   const uint32_t w_payment = 43;
   const uint32_t w_delivery = 4;
   const uint32_t w_order_status = 4;
   const uint32_t w_stock_level = 4;
 
   common::WorkerPool thread_pool_{static_cast<uint32_t>(num_threads_), {}};
+  BlockWriter writer;
 
   void ServerLoop(tpcc::Database *tpcc_db) {
     struct sockaddr_in sin;
@@ -130,7 +187,6 @@ class TpccLoader {
         blocks_written++;
         if (blocks_written % 500 == 0) printf("%u blocks written\n", blocks_written);
       }
-      SendReady(new_conn_fd);
       printf("data write completed\n");
       close(new_conn_fd);
     }
@@ -307,8 +363,6 @@ class TpccLoader {
   transaction::TransactionManager txn_manager{&buffer_pool_, true, LOGGING_DISABLED};
   bool first_call = true;
   uint64_t buf[1024];
-  char write_buf[1024 * 8];
-  uint32_t write_buf_head;
 
   void GCThreadLoop() {
     while (run_gc_) {
@@ -328,62 +382,26 @@ class TpccLoader {
     }
   }
 
-  void ResetWrite() {
-    std::memcpy(write_buf, "D", 1);
-    write_buf_head = 5; // type + len
-  }
-
-  void WriteAttribute(byte *entry, uint32_t size) {
-    std::memcpy(write_buf + write_buf_head, &size, sizeof(uint32_t));
-    write_buf_head += sizeof(uint32_t);
-    if (entry != nullptr)
-      std::memcpy(write_buf + write_buf_head, entry, size);
-    write_buf_head += size;
-  }
-
-  void WriteVarlenIntoRow(storage::VarlenEntry *varlen) {
-    if (varlen == nullptr) {
-      uint32_t size = 0;
-      std::memcpy(write_buf + write_buf_head, &size, sizeof(uint32_t));
-      write_buf_head += sizeof(uint32_t);
-      return;
-    }
-    uint32_t size = varlen->Size();
-    std::memcpy(write_buf + write_buf_head, &size, sizeof(uint32_t));
-    write_buf_head += sizeof(uint32_t);
-    std::memcpy(write_buf + write_buf_head, varlen->Content(), size);
-    write_buf_head += size;
-  }
-
-  void FlushRow(int fd) {
-    uint32_t size = write_buf_head - 5;
-    std::memcpy(write_buf + 1, &size, sizeof(uint32_t));
-    storage::PosixIoWrappers::WriteFully(fd, write_buf, write_buf_head);
-  }
-
-  void SendReady(int fd) {
-    write_buf_head = 5;
-    std::memcpy(write_buf, "Z", 1);
-    FlushRow(fd);
-  }
-
   void WriteColdBlock(tpcc::Database *tpcc_db, storage::RawBlock *block, int fd) {
     storage::DataTable *table = tpcc_db->order_line_table_->table_.data_table;
     const storage::BlockLayout &layout = table->accessor_.GetBlockLayout();
-    for (uint32_t i = 0; i < table->accessor_.GetArrowBlockMetadata(block).NumRecords(); i++) {
-      storage::TupleSlot slot(block, i);
-      if (!table->accessor_.Allocated(slot)) return;
-      ResetWrite();
-      for (uint16_t j = NUM_RESERVED_COLUMNS; j < layout.NumColumns(); j++) {
-        storage::col_id_t id(j);
-        byte *value = table->accessor_.AccessWithNullCheck(slot, id);
-        if (layout.IsVarlen(id))
-          WriteVarlenIntoRow(reinterpret_cast<storage::VarlenEntry *>(value));
-        else
-          WriteAttribute(value, layout.AttrSize(id));
-      }
-      FlushRow(fd);
+    uint32_t num_records = table->accessor_.GetArrowBlockMetadata(block).NumRecords();
+    writer.ResetWrite(num_records);
+    storage::col_id_t varlen_id(1);
+    auto *varlen_bitmap = table->accessor_.ColumnNullBitmap(block, varlen_id);
+    auto *varlen_values = reinterpret_cast<storage::VarlenEntry *>(table->accessor_.ColumnStart(block, varlen_id));
+    for (uint32_t i = 0; i < num_records; i++)
+      writer.WriteVarlen(varlen_bitmap->Test(i) ? varlen_values + i : nullptr);
+
+    for (uint16_t j = 2; j < layout.NumColumns(); j++) {
+      storage::col_id_t id(j);
+      uint32_t attr_size = layout.AttrSize(id);
+      auto *bitmap = table->accessor_.ColumnNullBitmap(block, id);
+      auto *values = table->accessor_.ColumnStart(block, id);
+      for (uint32_t i = 0; i < num_records; i++)
+        writer.WriteAttribute(bitmap->Test(i) ? values + i : nullptr, attr_size, j - 2);
     }
+    writer.FlushBatch(fd);
   }
 
   void WriteHotBlock(tpcc::Database *tpcc_db, storage::RawBlock *block, int fd) {
@@ -396,105 +414,22 @@ class TpccLoader {
     }
     auto *row = reinterpret_cast<storage::ProjectedRow *>(&buf);
     transaction::TransactionContext *txn = txn_manager.BeginTransaction();
+    writer.ResetWrite(block->insert_head_);
     for (uint32_t i = 0; i < layout.NumSlots(); i++) {
       storage::TupleSlot slot(block, i);
       bool visible = table->Select(txn, slot, row);
       if (!visible) continue;
-      ResetWrite();
       for (uint16_t j = 0; j < row->NumColumns(); j++) {
         byte *value = row->AccessWithNullCheck(j);
         storage::col_id_t id = row->ColumnIds()[j];
         if (layout.IsVarlen(id))
-          WriteVarlenIntoRow(reinterpret_cast<storage::VarlenEntry *>(value));
+          writer.WriteVarlen(reinterpret_cast<storage::VarlenEntry *>(value));
         else
-          WriteAttribute(value, layout.AttrSize(id));
+          writer.WriteAttribute(value, layout.AttrSize(id), j - 1);
       }
-      FlushRow(fd);
     }
+    writer.FlushBatch(fd);
   }
-
-  template<class IntType, class T>
-  void Append(T *builder, storage::ProjectedRow *row, uint16_t i) {
-    auto *int_pointer = row->AccessWithNullCheck(i);
-    if (int_pointer == nullptr)
-      auto status UNUSED_ATTRIBUTE = builder->AppendNull();
-    else
-      auto status1 UNUSED_ATTRIBUTE = builder->Append(*reinterpret_cast<IntType *>(int_pointer));
-  }
-
-//  std::shared_ptr<arrow::Table> MaterializeHotBlock(tpcc::Database *tpcc_db, storage::RawBlock *block) {
-//    storage::DataTable *table = tpcc_db->order_line_table_->table_.data_table;
-//    const storage::BlockLayout &layout = table->accessor_.GetBlockLayout();
-//    if (first_call) {
-//      auto initializer = storage::ProjectedRowInitializer::CreateProjectedRowInitializer(layout, layout.AllColumns());
-//      initializer.InitializeRow(&buf);
-//      first_call = false;
-//    }
-//    auto *row = reinterpret_cast<storage::ProjectedRow *>(&buf);
-//    transaction::TransactionContext *txn = txn_manager.BeginTransaction();
-//    arrow::Int32Builder o_id_builder;
-//    arrow::Int8Builder o_d_id_builder;
-//    arrow::Int8Builder o_w_id_builder;
-//    arrow::Int8Builder ol_number_builder;
-//    arrow::Int32Builder ol_i_id_builder;
-//    arrow::Int8Builder ol_supply_w_id_builder;
-//    arrow::Int64Builder ol_delivery_d_builder;
-//    arrow::Int8Builder ol_quantity_builder;
-//    arrow::DoubleBuilder ol_amount_builder;
-//    arrow::StringBuilder ol_dist_info_builder;
-//    for (uint32_t i = 0; i < layout.NumSlots(); i++) {
-//      storage::TupleSlot slot(block, i);
-//      bool visible = table->Select(txn, slot, row);
-//      if (!visible) continue;
-//      Append<uint32_t>(&o_id_builder, row, storage::DirtyGlobals::ol_o_id_insert_pr_offset);
-//      Append<uint8_t>(&o_d_id_builder, row, storage::DirtyGlobals::ol_d_id_insert_pr_offset);
-//      Append<uint8_t>(&o_w_id_builder, row, storage::DirtyGlobals::ol_w_id_insert_pr_offset);
-//      Append<uint8_t>(&ol_number_builder, row, storage::DirtyGlobals::ol_number_insert_pr_offset);
-//      Append<uint32_t>(&ol_i_id_builder, row, storage::DirtyGlobals::ol_i_id_insert_pr_offset);
-//      Append<uint8_t>(&ol_supply_w_id_builder, row, storage::DirtyGlobals::ol_supply_w_id_insert_pr_offset);
-//      Append<uint64_t>(&ol_delivery_d_builder, row, storage::DirtyGlobals::ol_delivery_d_insert_pr_offset);
-//      Append<uint8_t>(&ol_quantity_builder, row, storage::DirtyGlobals::ol_quantity_insert_pr_offset);
-//      Append<double>(&ol_amount_builder, row, storage::DirtyGlobals::ol_amount_insert_pr_offset);
-//
-//      auto *varlen_pointer = row->AccessWithNullCheck(storage::DirtyGlobals::ol_dist_info_insert_pr_offset);
-//      if (varlen_pointer == nullptr) {
-//        auto status UNUSED_ATTRIBUTE = ol_dist_info_builder.AppendNull();
-//      } else {
-//        auto *entry = reinterpret_cast<storage::VarlenEntry *>(varlen_pointer);
-//        auto status2 UNUSED_ATTRIBUTE =
-//            ol_dist_info_builder.Append(reinterpret_cast<const uint8_t *>(entry->Content()), entry->Size());
-//      }
-//    }
-//
-//    std::shared_ptr<arrow::Array> o_id, o_d_id, o_w_id, ol_number,
-//        ol_i_id, ol_supply_w_id, ol_delivery_d, ol_quantity, ol_amount, ol_dist_info;
-//    auto status UNUSED_ATTRIBUTE = o_id_builder.Finish(&o_id);
-//    auto status1 UNUSED_ATTRIBUTE = o_d_id_builder.Finish(&o_d_id);
-//    auto status2 UNUSED_ATTRIBUTE = o_w_id_builder.Finish(&o_w_id);
-//    auto status3 UNUSED_ATTRIBUTE = ol_number_builder.Finish(&ol_number);
-//    auto status4 UNUSED_ATTRIBUTE = ol_i_id_builder.Finish(&ol_i_id);
-//    auto status5 UNUSED_ATTRIBUTE = ol_supply_w_id_builder.Finish(&ol_supply_w_id);
-//    auto status6 UNUSED_ATTRIBUTE = ol_delivery_d_builder.Finish(&ol_delivery_d);
-//    auto status7 UNUSED_ATTRIBUTE = ol_quantity_builder.Finish(&ol_quantity);
-//    auto status8 UNUSED_ATTRIBUTE = ol_amount_builder.Finish(&ol_amount);
-//    auto status9 UNUSED_ATTRIBUTE = ol_dist_info_builder.Finish(&ol_dist_info);
-//
-//    std::vector<std::shared_ptr<arrow::Field>> schema_vector{arrow::field("o_id", arrow::uint32()),
-//                                                             arrow::field("o_d_id", arrow::uint8()),
-//                                                             arrow::field("o_w_id", arrow::uint8()),
-//                                                             arrow::field("ol_number", arrow::uint8()),
-//                                                             arrow::field("ol_i_id", arrow::uint32()),
-//                                                             arrow::field("ol_supply_w_id", arrow::uint8()),
-//                                                             arrow::field("ol_delivery_d", arrow::uint64()),
-//                                                             arrow::field("ol_quantity", arrow::uint8()),
-//                                                             arrow::field("ol_amount", arrow::float64()),
-//                                                             arrow::field("ol_dist_info", arrow::utf8())};
-//
-//    std::vector<std::shared_ptr<arrow::Array>> table_vector{o_id, o_d_id, o_w_id, ol_number,
-//                                                            ol_i_id, ol_supply_w_id, ol_delivery_d, ol_quantity,
-//                                                            ol_amount, ol_dist_info};
-//    return arrow::Table::Make(std::make_shared<arrow::Schema>(schema_vector), table_vector);
-//  }
 };
 }
 
@@ -542,71 +477,73 @@ struct ArrowBufferBuilder {
   arrow::StringBuilder ol_dist_info_builder;
 };
 
-#define READBUF_SIZE (1024 * 8)
+char read_buf[terrier::common::Constants::BLOCK_SIZE * 50];
+#define READBUF_SIZE (terrier::common::Constants::BLOCK_SIZE * 10)
 #define SUCCESS 0
 #define NEED_MORE 1
 #define DONE 2
 struct ReadBuffer {
   template<class T>
   T ReadValue() {
-    T val = *reinterpret_cast<T *>(buffer + read_head);
+    T val = *reinterpret_cast<T *>(read_buf + read_head);
     read_head += sizeof(T);
     return val;
   }
 
-  int ReadRow(ArrowBufferBuilder &builder) {
-    if (read_head + 5 > size) return NEED_MORE;
-    char packet_type = ReadValue<char>();
-    if (packet_type == 'Z') return DONE;
-    if (packet_type != 'D') throw std::runtime_error("malformed packet");
-    uint32_t packet_size = ReadValue<uint32_t>();
-    if (read_head + packet_size > size) {
-      read_head -= 5;
+  int ReadBatch(ArrowBufferBuilder &builder) {
+    if (read_head + 2 * sizeof(uint32_t) > size) return has_more ? NEED_MORE : DONE;
+    uint32_t batchsize = ReadValue<uint32_t>(), total_size = ReadValue<uint32_t>();
+    if (read_head + total_size > size) {
+      read_head -= 2 * sizeof(uint32_t);
       return NEED_MORE;
     }
-
-    uint32_t varlen_size = ReadValue<uint32_t>();
-    auto status UNUSED_ATTRIBUTE = builder.ol_dist_info_builder.Append(buffer + read_head, varlen_size);
-    read_head += varlen_size;
-    read_head += sizeof(uint32_t);
-    auto status1 UNUSED_ATTRIBUTE = builder.ol_delivery_d_builder.Append(ReadValue<uint64_t>());
-    read_head += sizeof(uint32_t);
-    auto status2 UNUSED_ATTRIBUTE = builder.ol_amount_builder.Append(ReadValue<uint64_t>());
-    read_head += sizeof(uint32_t);
-    auto status3 UNUSED_ATTRIBUTE = builder.o_id_builder.Append(ReadValue<uint32_t>());
-    read_head += sizeof(uint32_t);
-    auto status4 UNUSED_ATTRIBUTE = builder.ol_i_id_builder.Append(ReadValue<uint32_t>());
-    read_head += sizeof(uint32_t);
-    auto status5 UNUSED_ATTRIBUTE = builder.o_d_id_builder.Append(ReadValue<uint8_t>());
-    read_head += sizeof(uint32_t);
-    auto status6 UNUSED_ATTRIBUTE = builder.o_w_id_builder.Append(ReadValue<uint8_t>());
-    read_head += sizeof(uint32_t);
-    auto status7 UNUSED_ATTRIBUTE = builder.ol_number_builder.Append(ReadValue<uint8_t>());
-    read_head += sizeof(uint32_t);
-    auto status8 UNUSED_ATTRIBUTE = builder.ol_supply_w_id_builder.Append(ReadValue<uint8_t>());
-    read_head += sizeof(uint32_t);
-    auto status9 UNUSED_ATTRIBUTE = builder.ol_quantity_builder.Append(ReadValue<uint8_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status1 UNUSED_ATTRIBUTE = builder.ol_delivery_d_builder.Append(ReadValue<uint64_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status2 UNUSED_ATTRIBUTE = builder.ol_amount_builder.Append(ReadValue<uint64_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status3 UNUSED_ATTRIBUTE = builder.o_id_builder.Append(ReadValue<uint32_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status4 UNUSED_ATTRIBUTE = builder.ol_i_id_builder.Append(ReadValue<uint32_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status5 UNUSED_ATTRIBUTE = builder.o_d_id_builder.Append(ReadValue<uint8_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status6 UNUSED_ATTRIBUTE = builder.o_w_id_builder.Append(ReadValue<uint8_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status7 UNUSED_ATTRIBUTE = builder.ol_number_builder.Append(ReadValue<uint8_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status8 UNUSED_ATTRIBUTE = builder.ol_supply_w_id_builder.Append(ReadValue<uint8_t>());
+    for (uint i = 0; i < batchsize; i++)
+      auto status9 UNUSED_ATTRIBUTE = builder.ol_quantity_builder.Append(ReadValue<uint8_t>());
+    for (uint i = 0; i < batchsize; i++) {
+      uint32_t varlen_size = ReadValue<uint32_t>();
+      auto status UNUSED_ATTRIBUTE = builder.ol_dist_info_builder.Append(read_buf + read_head, varlen_size);
+      read_head += varlen_size;
+    }
     return SUCCESS;
   }
 
   void ShiftToHead() {
     for (uint i = 0; i < size - read_head; i++)
-      buffer[i] = buffer[read_head + i];
+      read_buf[i] = read_buf[read_head + i];
     size = size - read_head;
     read_head = 0;
   }
 
   void RefillBuffer(int sock) {
     while (size < READBUF_SIZE) {
-      auto read_bytes = read(sock, buffer + size, READBUF_SIZE - size);
-      if (read_bytes == 0) return;
+      auto read_bytes = read(sock, read_buf + size, READBUF_SIZE - size);
+      if (read_bytes == 0) {
+        has_more = false;
+        return;
+      }
       size += read_bytes;
     }
   }
 
   uint32_t size = 0;
   uint32_t read_head = 0;
-  char buffer[READBUF_SIZE]{};
+  bool has_more = true;
 };
 
 int sock_connect(const char *servername, int port) {
@@ -698,7 +635,7 @@ int main(int argc, char *argv[]) {
   ArrowBufferBuilder builder;
   uint32_t rows_read = 0;
   while (true) {
-    int result = reader.ReadRow(builder);
+    int result = reader.ReadBatch(builder);
     if (result == DONE) break;
     if (result == NEED_MORE) {
       reader.ShiftToHead();
@@ -711,7 +648,7 @@ int main(int argc, char *argv[]) {
   builder.Build();
   std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
   fprintf(stdout,
-          "Client side TOTAL duration: %ld\n",
+          "Client side TOTAL duration: %lld\n",
           std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
   return 0;
 }
