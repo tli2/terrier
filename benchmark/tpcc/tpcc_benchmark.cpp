4275a95690e554c86b0ddad27c72c09898163552
Tianyu Li <tli2@andrew.cmu.edu>
2019-10-17 14:12:00 -0400

    setup without compaction

--- benchmark/tpcc/tpcc_benchmark.cpp
+++ benchmark/tpcc/tpcc_benchmark.cpp
@@ -39,8 +39,8 @@ class TPCCBenchmark : public benchmark::Fixture {
 
   void StartGC(transaction::TransactionManager *const txn_manager) {
 
-    gc_ = new storage::GarbageCollector(txn_manager, &access_observer_);
-//    gc_ = new storage::GarbageCollector(txn_manager);
+//    gc_ = new storage::GarbageCollector(txn_manager, &access_observer_);
+    gc_ = new storage::GarbageCollector(txn_manager);
     run_gc_ = true;
     gc_thread_ = std::thread([this] { GCThreadLoop(); });
   }
@@ -133,8 +133,8 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
   thread_pool_.Startup();
 
   // we need transactions, TPCC database, and GC
-//  log_manager_ = new storage::LogManager(LOG_FILE_NAME, &buffer_pool_);
-  transaction::TransactionManager txn_manager(&buffer_pool_, true, LOGGING_DISABLED);
+  log_manager_ = new storage::LogManager(LOG_FILE_NAME, &buffer_pool_);
+  transaction::TransactionManager txn_manager(&buffer_pool_, true, log_manager_);
   auto tpcc_builder = tpcc::Builder(&block_store_);
 
   // random number generation is slow, so we precompute the args
@@ -187,10 +187,10 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
     access_observer_ = storage::AccessObserver(&compactor_);
 
     tpcc::Loader::PopulateDatabase(&txn_manager, &generator_, tpcc_db, workers);
-//    log_manager_->Process();  // log all of the Inserts from table creation
+    log_manager_->Process();  // log all of the Inserts from table creation
     StartGC(&txn_manager);
 
-//    StartLogging();
+    StartLogging();
     std::this_thread::sleep_for(std::chrono::seconds(1));  // Let GC clean up
     StartCompactor(&txn_manager);
     // define the TPCC workload
@@ -239,28 +239,28 @@ BENCHMARK_DEFINE_F(TPCCBenchmark, Basic)(benchmark::State &state) {
       }
       thread_pool_.WaitUntilAllFinished();
       printf("transactions all submitted\n");
-//      EndLogging();
+      EndLogging();
     }
     state.SetIterationTime(static_cast<double>(elapsed_ms) / 1000.0);
     // cleanup
     EndCompactor();
     EndGC();
-    printf("history table:\n");
-    tpcc_db->history_table_->table_.data_table->InspectTable();
-    printf("item table:\n");
-    tpcc_db->item_table_->table_.data_table->InspectTable();
-    printf("order table:\n");
-    tpcc_db->order_table_->table_.data_table->InspectTable();
-    printf("order_line table:\n");
-    tpcc_db->order_line_table_->table_.data_table->InspectTable();
-    printf("\n\n\n");
-    printf("total number of transactions: %u\n", num_precomputed_txns_per_worker_ * num_threads_);
-    printf("number of transactions stalled: %u\n", storage::DirtyGlobals::blocked_transactions.load());
-    uint32_t aborted = 0;
-    for (auto &entry : precomputed_args)
-      for (auto &arg : entry)
-        aborted += arg.aborted;
-    printf("number of transactions aborted: %u\n", aborted);
+//    printf("history table:\n");
+//    tpcc_db->history_table_->table_.data_table->InspectTable();
+//    printf("item table:\n");
+//    tpcc_db->item_table_->table_.data_table->InspectTable();
+//    printf("order table:\n");
+//    tpcc_db->order_table_->table_.data_table->InspectTable();
+//    printf("order_line table:\n");
+//    tpcc_db->order_line_table_->table_.data_table->InspectTable();
+//    printf("\n\n\n");
+//    printf("total number of transactions: %u\n", num_precomputed_txns_per_worker_ * num_threads_);
+//    printf("number of transactions stalled: %u\n", storage::DirtyGlobals::blocked_transactions.load());
+//    uint32_t aborted = 0;
+//    for (auto &entry : precomputed_args)
+//      for (auto &arg : entry)
+//        aborted += arg.aborted;
+//    printf("number of transactions aborted: %u\n", aborted);
     delete tpcc_db;
     delete log_manager_;
   }
