2934cadddfceea908e630bdc6cdbd1424cb715af
Tianyu Li <tli2@andrew.cmu.edu>
2019-10-19 20:39:53 -0400

    add double conversion lookup

--- cmake_modules/FindArrow.cmake
+++ cmake_modules/FindArrow.cmake
@@ -14,17 +14,23 @@ find_library(ARROW_PYTHON
         HINTS /usr/local/lib
         )
 
+find_library(DOUBLE_CONVERSION
+        NAMES libdouble-conversion.a
+        HINTS /usr/local/lib)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Arrow DEFAULT_MSG
         ARROW_LIBRARIES
         ARROW_INCLUDE_DIR
         ARROW_PYTHON
+        DOUBLE_CONVERSION
         )
 
 mark_as_advanced(
         ARROW_LIBRARIES
         ARROW_INCLUDE_DIR
         ARROW_PYTHON
+        DOUBLE_CONVERSION
 )
 
 message(STATUS "Found arrow (include: ${ARROW_INCLUDE_DIRS}, library: ${ARROW_LIBRARIES})")
