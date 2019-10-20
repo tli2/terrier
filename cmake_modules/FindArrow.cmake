1d4971ddbac1935dec318245381083046e7563e6
Tianyu Li <tli2@andrew.cmu.edu>
2019-10-19 22:30:03 -0400

    remove double conversion cmake

--- cmake_modules/FindArrow.cmake
+++ cmake_modules/FindArrow.cmake
@@ -4,33 +4,27 @@ find_library(ARROW_LIBRARIES
         HINTS /usr/local/lib
         )
 
-find_path(ARROW_INCLUDE_DIR
-        NAMES arrow/api.h
-        HINTS /usr/local/include
-        )
-
 find_library(ARROW_PYTHON
         NAMES libarrow_python.a
         HINTS /usr/local/lib
         )
 
-find_library(DOUBLE_CONVERSION
-        NAMES libdouble-conversion.a
-        HINTS /usr/local/lib)
+find_path(ARROW_INCLUDE_DIR
+        NAMES arrow/api.h
+        HINTS /usr/local/include
+        )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Arrow DEFAULT_MSG
         ARROW_LIBRARIES
         ARROW_INCLUDE_DIR
         ARROW_PYTHON
-        DOUBLE_CONVERSION
         )
 
 mark_as_advanced(
         ARROW_LIBRARIES
         ARROW_INCLUDE_DIR
         ARROW_PYTHON
-        DOUBLE_CONVERSION
 )
 
 message(STATUS "Found arrow (include: ${ARROW_INCLUDE_DIRS}, library: ${ARROW_LIBRARIES})")
