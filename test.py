4105e5d2c70a28b373b3c1bcfc139afea3c9ea55
Tianyu Li <tli2@andrew.cmu.edu>
2019-10-20 20:07:44 -0400

    Add fake tf program

---
+++ test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras import layers
+from keras.layers import activations
+from keras import optimizers
+from keras import models
+from keras import losses
+from keras import backend as K
+import tensorflow as tf
+import tensorflow_io.arrow as arrow_io
+import pyarrow
+import client
+
+class StupidNetwork:
+  def __init__(self):
+    self.model = Sequential()
+    self.model.add(layers.Dense(20, input_dim=8, activation=activations.relu))
+    self.model.add(layers.Dense(10), activation=activations.relu)
+    self.model.add(layers.Dense(10), activation=activations.relu)
+    self.model.add(layers.Dense(1))
+    self.model.compile(loss=losses.mean_squared_error, optimizer=optimizers.Adam(lr=1e-4))
+
+  def train(self, pandas_df):
+    dataset = arrow_io.ArrowDataset.from_pandas(df)
+    dataset = dataset.shuffle(buffer_size=1024).batch(8)
+    self.model.fit(dataset)
+
+  def eval(self, pandas_df):
+    print("Evaluating...\n")
+    model.evaluate(arrow_io.ArrowDataset.from_pandas(df).batch(8))
+
+network = StupidNetwork()
+t = client.read_table("snode", 0.0)
+network.train(t.to_pandas())
+network.eval(t.to_pandas())
+
